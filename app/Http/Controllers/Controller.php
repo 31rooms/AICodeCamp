821d8de7beabab648f4129bc9e2f76d8e535d355
Cesar Perez <cpjbugg.8@gmail.com>
2024-09-01 19:05:03 -0600

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *    title="Cesar Perez :: Tu Colegio Ideal ",
+ *    version="1.0.0",
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="bearerAuth",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+
+ */
+abstract class Controller
+{
+    //
+}
